d49eba5dbdcacb37819f6676609d5d6971d21b5d
Mitch Malinin <mitchmalinin@gmail.com>
2022-01-26 21:43:01 -0500

    script tags

--- pages/_app.js
+++ pages/_app.js
@@ -6,32 +6,36 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <Script
-          strategy="lazyOnload"
+        <script
+          async
           src="https://www.googletagmanager.com/gtag/js?id=UA-218073064-1"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  
+                  gtag('config', 'UA-218073064-1');
+                  gtag('config', 'G-FPEJ8734NG');
+                  `,
+          }}
         />
-        <Script strategy="lazyOnload">
-          {`
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-           
-           gtag('config', 'UA-218073064-1');
-           gtag('config', 'G-FPEJ8734NG');
-        `}
-        </Script>
-        <Script strategy="lazyOnload">
-          {`
-           (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2798402,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        `}
-        </Script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2798402,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+         `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
