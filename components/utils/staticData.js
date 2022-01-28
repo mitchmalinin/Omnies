0ddd1e4d4f93bb311d3fbbf7fb2392c5c3ed659c
Mitch Malinin <mitchmalinin@gmail.com>
2022-01-27 19:45:25 -0500

    img sizes

--- components/utils/staticData.js
+++ components/utils/staticData.js
@@ -9,19 +9,19 @@ export const teamMembers = [
     name: "Mr.Wzrd",
     twitter: "#",
     role: "Web Wizard",
-    img: "/images/mystics.png",
+    img: "/images/leaders/BKv4.jpg",
   },
   {
     name: "Mr.Wzrd",
     twitter: "#",
     role: "Web Wizard",
-    img: "/images/mystics.png",
+    img: "/images/leaders/BKv5.png",
   },
   {
     name: "Mr.Wzrd",
     twitter: "#",
     role: "Web Wizard",
-    img: "/images/mystics.png",
+    img: "/images/leaders/BKv6.png",
   },
   {
     name: "Mr.Wzrd",
